37610c601d4e3c18a6e90f65d115ebe7d8d3c44b
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-18 15:06:52 -0700

    Add `/1,2,3` retrieve route instead of list `ids[]=`

--- zc_common/remote_resource/views.py
+++ zc_common/remote_resource/views.py
@@ -13,31 +13,28 @@ from zc_common.remote_resource.serializers import ResourceIdentifierObjectSerial
 
 class ModelViewSet(viewsets.ModelViewSet):
     """
-    This class overwrites the ModelViewSet's list method, which handles
-    requests made to the collection's base endpoint (/collection/), in
-    order to fulfill requests made for multiple resources by chaning together
-    query parameters in the format of /collection?ids[]=1&ids[]=3&ids[]=5.
-
-    Inheriting from this class will, with no extra action required, properly
-    handle requests made to /collection/ as well as /collection?ids[]=1&ids[]=3.
+    This class overwrites the ModelViewSet's retrieve method, which normally provides
+    a serialized representation of a single object with the ID provided in the url
+    parameter such as `/collection/1`. This allows the retrieve route to serialize any
+    arbitrary list of ids provided to it, comma-separated, such as `/collection/1,2,3`.
     """
-    def list(self, request, *args, **kwargs):
-        if hasattr(request, 'query_params'):
-            ids = self.request.query_params.getlist('ids[]')
-            if ids:
-                try:
-                    queryset = self.filter_queryset(self.get_queryset().filter(pk__in=ids))
-                except (ValueError, IntegrityError):
-                    raise Http404
+    def retrieve(self, request, *args, **kwargs):
+        pks = kwargs.pop('pk', None)
+        if pks:
+            pks = pks.split(',')
+            try:
+                queryset = self.filter_queryset(self.get_queryset().filter(pk__in=pks))
+            except (ValueError, IntegrityError):
+                raise Http404
 
-                page = self.paginate_queryset(queryset)
-                if page is not None:
-                    serializer = self.get_serializer(page, many=True)
-                    return self.get_paginated_response(serializer.data)
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
 
-                serializer = self.get_serializer(queryset, many=True)
-                return Response(serializer.data)
-        return super(ModelViewSet, self).list(request, *args, **kwargs)
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        return super(ModelViewSet, self).retrieve(request, *args, **kwargs)
 
 
 class RelationshipView(RelationshipView):
